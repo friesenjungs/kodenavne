28f400c5861495c83f3f8e7cf415a8021254cf03
Kevin Wiesner <50447092+4KevR@users.noreply.github.com>
2022-06-14 10:32:44 +0200

    deepsource changes models.py

--- config.py
+++ config.py
@@ -7,6 +7,6 @@ TESTING = False
 CSRF_ENABLED = True
 STATIC_FOLDER = 'static'
 TEMPLATES_FOLDER = 'templates'
-SQLALCHEMY_DATABASE_URI = f"postgresql://{os.environ['POSTGRES_USER']}:{os.environ['POSTGRES_PASSWORD']}" \
+SQLALCHEMY_DATABASE_URI = f"postgresql://{os.environ['POSTGRES_USER']}:" \
                           f"@db:5432/{os.environ['POSTGRES_DB']}"
 SQLALCHEMY_TRACK_MODIFICATIONS = False
