d196c3b823afa269d70cba20932caeb0558302cb
Julian <66846059+Julian702@users.noreply.github.com>
2022-06-20 11:30:38 +0200

    added dark mode

--- static/js/base.js
+++ static/js/base.js
@@ -2,3 +2,10 @@ function joinRoom(){
     let code = document.getElementById("idCodeInput").value;
     code ? window.open("/room/" + code, "_self") : console.log("no code entered");
 }
+
+function toggleTheme(){
+    console.log("Toggle");
+    document.getElementById("idBody").classList.toggle('bg-dark');
+    document.getElementById("idBody").classList.toggle('text-light');
+    document.getElementById("idTheme").classList.toggle('bi-sun')
+}
