6558fbf12ead177578bcc188cbbca1381466e4aa
Julian <66846059+Julian702@users.noreply.github.com>
2022-06-24 10:46:48 +0200

    changed routing for joining room with code some small visual changes

--- static/js/base.js
+++ static/js/base.js
@@ -1,18 +1,12 @@
-const joinRoom = () => {
-    const code = document.getElementById("idCodeInput").value;
-    if(code) window.open(`/room/${code}`, "_self");
-}
-
 const toggleTheme = () => {
     const body = document.getElementById("idBody");
     body.classList.toggle('bg-dark');
     body.classList.toggle('text-light');
     document.getElementById("idTheme").classList.toggle('bi-sun');
-    localStorage.setItem("theme", body.classList.contains("bg-dark") ? "dark" : "light");  
+    localStorage.setItem("theme", body.classList.contains("bg-dark") ? "dark" : "light");
 }
 
 document.addEventListener("DOMContentLoaded", () => {
-    document.getElementById("idJoinRoomBtn").addEventListener("click", joinRoom);
     document.getElementById("idToggleThemeBtn").addEventListener("click", toggleTheme);
-    if(localStorage.getItem("theme") === "light") toggleTheme();
+    if (localStorage.getItem("theme") === "light") toggleTheme();
 });
