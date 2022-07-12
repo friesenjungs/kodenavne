2d2247755fcac522978a3cb3f996fdb71d275f8d
Julian <66846059+Julian702@users.noreply.github.com>
2022-07-12 23:40:41 +0200

    added winning animation

--- static/js/room.js
+++ static/js/room.js
@@ -18,6 +18,125 @@ const getStyle = {
 	6: 'background-color: black; border-color: white !important; border-width: medium !important; color: white' // balck card
 }
 
+const particle = {
+	"emitters": {
+		"position": {
+			"x": 50,
+			"y": 100
+		},
+		"rate": {
+			"quantity": 5,
+			"delay": 0.15
+		}
+	},
+	"particles": {
+		"color": {
+			"value": [
+				"#1E00FF",
+				"#FF0061",
+				"#E1FF00",
+				"#00FF9E"
+			]
+		},
+		"move": {
+			"decay": 0.05,
+			"direction": "top",
+			"enable": true,
+			"gravity": {
+				"enable": true
+			},
+			"outModes": {
+				"top": "none",
+				"default": "destroy"
+			},
+			"speed": {
+				"min": 75,
+				"max": 150
+			}
+		},
+		"number": {
+			"value": 0
+		},
+		"opacity": {
+			"value": 1
+		},
+		"rotate": {
+			"value": {
+				"min": 0,
+				"max": 360
+			},
+			"direction": "random",
+			"animation": {
+				"enable": true,
+				"speed": 30
+			}
+		},
+		"tilt": {
+			"direction": "random",
+			"enable": true,
+			"value": {
+				"min": 0,
+				"max": 360
+			},
+			"animation": {
+				"enable": true,
+				"speed": 30
+			}
+		},
+		"size": {
+			"value": 3,
+			"animation": {
+				"enable": true,
+				"startValue": "min",
+				"count": 1,
+				"speed": 16,
+				"sync": true
+			}
+		},
+		"roll": {
+			"darken": {
+				"enable": true,
+				"value": 25
+			},
+			"enlighten": {
+				"enable": true,
+				"value": 25
+			},
+			"enable": true,
+			"speed": {
+				"min": 5,
+				"max": 15
+			}
+		},
+		"wobble": {
+			"distance": 30,
+			"enable": true,
+			"speed": {
+				"min": -7,
+				"max": 7
+			}
+		},
+		"shape": {
+			"type": [
+				"circle",
+				"square",
+				"triangle",
+				"polygon"
+			],
+			"options": {
+				"polygon": [
+					{
+						"sides": 5
+					},
+					{
+						"sides": 6
+					}
+				]
+			}
+		}
+	}
+}
+
 // show Bootstrap Toast to communicate with user
 const showToast = (title, message, icon, subtitle) => {
 	const toast =
@@ -258,11 +377,11 @@ window.onload = () => {
 	});
 
 	socket.on('end game', data => {
-		console.log();
 		if (data.winner_team) {
 			document.getElementById("idGameStatusMessage").firstChild.nodeValue = `Team ${data.winner_team} won the game!`;
 		} else if (data.looser_team) {
 			document.getElementById("idGameStatusMessage").firstChild.nodeValue = `Team ${data.looser_team} lost the game!`;
 		}
+		tsParticles.load("tsparticles", particle);
 	});
 }
